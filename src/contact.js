3fb444f1b9378414c5980036ab2a1f974aa1cfc2
Gonzalo <gonzalopiombi@gmail.com>
2021-09-02 16:19:25 -0300

    Add schedule and image to contact page.

--- src/contact.js
+++ src/contact.js
@@ -12,9 +12,20 @@ function createContactPage() {
 
     fillInformation(information);
 
+    //Schedule
+    schedule(information);
+
     //Image container
     const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container')
     container.appendChild(imageContainer);
+
+    const contactImage = document.createElement('img');
+    contactImage.classList.add('contact-image');
+    contactImage.setAttribute('src', 'images/restaurant.jpg');
+
+    imageContainer.appendChild(contactImage);
+
 }
 
 function fillInformation(information) {
@@ -25,16 +36,28 @@ function fillInformation(information) {
 
     //Phone info.
     const phone = document.createElement('div');
+    phone.classList.add('phone-address');
     information.appendChild(phone);
     phone.appendChild(createIcon('phoneIcon', '35px', 'phone'));
     phone.appendChild(createText('030-345-6'));
 
-    //Adress info.
-    const adress = document.createElement('div');
-    information.appendChild(adress);
-    adress.appendChild(createIcon('locationIcon', '35px', 'place'));
-    adress.appendChild(createText('123 Fake Street'));
+    //Address info.
+    const address = document.createElement('div');
+    address.classList.add('phone-address');
+    information.appendChild(address);
+    address.appendChild(createIcon('locationIcon', '35px', 'place'));
+    address.appendChild(createText('123 Fake Street'));
 }
 
+function schedule(information) {
+    const schedule = document.createElement('div');
+    schedule.classList.add('schedule');
+    information.appendChild(schedule);
+
+    schedule.appendChild(createIcon('scheduleIcon', '35px', 'schedule'));
+    schedule.appendChild(createText('Mon: Closed'));
+    schedule.appendChild(createText('Tue-Thu: 9am to 1am'));
+    schedule.appendChild(createText('Fri-Sun: 9am to 2am'));
+}
 
 export default createContactPage
