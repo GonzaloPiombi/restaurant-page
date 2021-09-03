96b5291eb665294e534515d694dd100690e141ce
Gonzalo <gonzalopiombi@gmail.com>
2021-09-03 12:39:32 -0300

    Create menu page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createHeader } from './page';
 import { createHomePage } from './home';
 import createContactPage from './contact';
+import createMenu from './menu';
 
 function addLinks() {
     const links = document.querySelectorAll('a');
@@ -17,6 +18,7 @@ function addLinks() {
             } else if (e.currentTarget.textContent === 'Home') {
                 document.querySelector('.contact').style = 'display: none';
                 document.querySelector('main').style = 'display: flex';
+
                 link.classList.add('active-tab');
                 document.querySelectorAll('a')[1].classList.remove('active-tab');
                 document.querySelectorAll('a')[2].classList.remove('active-tab');
@@ -27,5 +29,6 @@ function addLinks() {
 
 createHeader();
 createHomePage();
+createMenu();
 createContactPage();
 addLinks();
--- src/menu.js
+++ src/menu.js
@@ -2,11 +2,61 @@ import { createText } from './home';
 import { createIcon } from './page';
 
 function createMenu() {
-    const menuContainer = document.createElement('div');
-    menuContainer.classList.add('menu');
-    document.querySelector('#content').appendChild(menuContainer);
+    const menuOutsideContainer = document.createElement('div');
+    menuOutsideContainer.classList.add('menu-container');
+    document.querySelector('#content').appendChild(menuOutsideContainer);
 
+    menuOutsideContainer.appendChild(createIcon('forkAndKnifeIcon', '40px', 'restaurant_menu'));
+    const title = document.createElement('h2');
+    title.textContent = 'Menu';
+    menuOutsideContainer.appendChild(title);
+    menuOutsideContainer.appendChild(createIcon('forkAndKnifeIcon', '40px', 'restaurant_menu'));
 
+    const menuInsideContainer = document.createElement('div');
+    menuInsideContainer.classList.add('menu');
+    menuOutsideContainer.appendChild(menuInsideContainer);
+
+    pastaInfo(menuInsideContainer);
+    sauceInfo(menuInsideContainer);
+}
+
+function pastaInfo(menuInsideContainer) {
+    const pastaContainer = document.createElement('div');
+    //Class
+    menuInsideContainer.appendChild(pastaContainer);
+
+    //Title
+    const title = document.createElement('h3');
+    title.textContent = 'Our pastas';
+    pastaContainer.appendChild(title);
+
+    pastaContainer.appendChild(createIcon('pastaIcon', '35px', 'dinner_dining'));
+
+    pastaContainer.appendChild(createText('Spaghetti'));
+    pastaContainer.appendChild(createText('Farfalle'));
+    pastaContainer.appendChild(createText('Rigatoni'));
+    pastaContainer.appendChild(createText('Ravioli'));
+    pastaContainer.appendChild(createText('Gnocchi'));
+    pastaContainer.appendChild(createText('Lasagna'));
+}
+
+function sauceInfo(menuInsideContainer) {
+    const sauceContainer = document.createElement('div');
+    //Class
+    menuInsideContainer.appendChild(sauceContainer);
+
+    const title = document.createElement('h3');
+    title.textContent = 'Our sauces';
+    sauceContainer.appendChild(title);
+
+    sauceContainer.appendChild(createIcon('sauceIcon', '35px', 'water_drop'));
+
+    sauceContainer.appendChild(createText('Bolognese'));
+    sauceContainer.appendChild(createText('Marinara'));
+    sauceContainer.appendChild(createText('Carbonara'));
+    sauceContainer.appendChild(createText('Pesto'));
+    sauceContainer.appendChild(createText('Alfredo'));
+    sauceContainer.appendChild(createText('Frutti di mare'));
 }
 
 export default createMenu;
