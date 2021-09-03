b97b974fa8f9db0d8133709f01800fc70c07c45d
Gonzalo <gonzalopiombi@gmail.com>
2021-09-03 11:31:39 -0300

    Build tab changing logic.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import { createHeader } from './page';
 import { createHomePage } from './home';
 import createContactPage from './contact';
 
+function addLinks() {
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            if (e.currentTarget.textContent === 'Contact') {
+                document.querySelector('.contact').style = 'display: flex';
+                document.querySelector('main').style = 'display: none';
+
+                link.classList.add('active-tab');
+                document.querySelectorAll('a')[0].classList.remove('active-tab');
+                document.querySelectorAll('a')[1].classList.remove('active-tab');
+            } else if (e.currentTarget.textContent === 'Home') {
+                document.querySelector('.contact').style = 'display: none';
+                document.querySelector('main').style = 'display: flex';
+                link.classList.add('active-tab');
+                document.querySelectorAll('a')[1].classList.remove('active-tab');
+                document.querySelectorAll('a')[2].classList.remove('active-tab');
+            }
+        });
+    });
+}
+
 createHeader();
 createHomePage();
 createContactPage();
+addLinks();
